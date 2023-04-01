e07e20e64a615301bd42eb860251a6d177f7cf4d
turtlelazy <ishraqmahid@gmail.com>
2023-03-31 21:51:45 -0400

    basic setup of flask frame

---
+++ __init__.py
@@ -0,0 +1,50 @@
+from flask import session
+from flask import request, render_template, Flask, redirect
+from os import urandom
+app = Flask(__name__)
+debug = True
+#why do we need secret keys? For this project it is not really necessary but...
+#https://stackoverflow.com/questions/22463939/demystify-flask-app-secret-key
+#I believe in a real life environment we should store the key somewhere safe in case of server crash
+app.secret_key = urandom(24)
+#   Things to work on:
+#   Implimentationationation
+#   Adding error messages and try and fails
+
+#helper method
+
+
+@app.route("/", methods=['GET'])
+def welcome():
+    '''
+    Welcome Page
+    '''
+    return render_template("main.html")
+
+
+@app.route("/sheet/upload", methods=['GET', 'POST'])
+def welcome():
+    '''
+    Page for uploading sheet music
+    '''
+    return render_template("upload.html")
+
+
+@app.route("/sheet/process", methods=['GET', 'POST'])
+def welcome():
+    '''
+    Page for uploading sheet music
+    '''
+    return render_template("upload.html")
+
+def main():
+    """
+    false if this file imported as module
+    debugging enabled
+    """
+    app.debug = True
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
